6ec2b5acfc7f31d636373fd31527f292f49f76f1
NAVYAGUTA1403 <navyagupta1406@gmail.com>
2024-03-19 15:50:34 +0530

    First Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '50b3f32d-179d-42f8-b023-a654627a1bcd';
+export const MODAL_CLOSE_SEC = 2.5;
